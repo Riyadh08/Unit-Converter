4d9b48f7c7e51555f261b094008dce51aa262c85
ryad <robiulriyadh66@gmail.com>
2024-11-07 14:26:06 +0600

    first commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Gaming Lab on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
